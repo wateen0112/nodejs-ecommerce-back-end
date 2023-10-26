009270493955ef03bcbbe1a491446d666d31c100
wateen0112 <65304439+wateen0112@users.noreply.github.com>
2023-10-26 19:55:18 +0300

    Update not-found.js

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,3 +1,9 @@
+aconst notFound = (req, res) => res.status(404).send({
+    msg:'not found',
+    full_path:req.originalUrl
+})
+
+module.exports = notFound
 const notFound = (req, res) => res.status(404).send('Route does not exist')
 
 module.exports = notFound
